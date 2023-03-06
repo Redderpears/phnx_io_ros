5125ca1798359428247c4f4a414c71d9c98ff493
Jordan Krasan <jkrasan@umich.edu>
2023-03-06 17:37:40 -0500

    Got gz_io_ros running with concurrent queues

--- gz_io_ros/include/gz_io_ros/gz_io_ros.hpp
+++ gz_io_ros/include/gz_io_ros/gz_io_ros.hpp
@@ -8,7 +8,17 @@
 #include "message_filters/sync_policies/approximate_time.h"
 
 namespace gir {
-    typedef message_filters::sync_policies::ApproximateTime<nav_msgs::msg::Odometry, geometry_msgs::msg::TwistStamped> sync_policy;
+
+    static const geometry_msgs::msg::Twist zero_twist = [] {
+        geometry_msgs::msg::Twist t;
+        t.angular.x = 0.0;
+        t.angular.y = 0.0;
+        t.angular.z = 0.0;
+        t.linear.x = 0.0;
+        t.linear.y = 0.0;
+        t.linear.z = 0.0;
+        return t;
+    }();
 
     class GzIoRos : public rclcpp::Node {
     public:
@@ -18,12 +28,24 @@ namespace gir {
         std::optional<std::shared_ptr<rclcpp::Publisher<ackermann_msgs::msg::AckermannDrive>>>
                 _odom_acks_pub = std::nullopt;
 
+        rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr _twist_sub;
+        rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr _odom_sub;
+
+        std::list<geometry_msgs::msg::Twist::SharedPtr> twist_queue;
+        std::list<nav_msgs::msg::Odometry::SharedPtr> odom_queue;
+
+
         double _max_throttle_speed{};
         double _max_braking_speed{};
         double _wheelbase{};
 
-        void convert_data(nav_msgs::msg::Odometry::ConstSharedPtr odom,
-                          geometry_msgs::msg::TwistStamped::ConstSharedPtr cmd_vel);
+        void
+        convert_data(nav_msgs::msg::Odometry::ConstSharedPtr odom, geometry_msgs::msg::Twist::ConstSharedPtr twist);
+
+        void odom_cb(nav_msgs::msg::Odometry::SharedPtr odom);
+
+        void twist_cb(geometry_msgs::msg::Twist::SharedPtr twist);
+
         double convert_trans_rot_vel_to_steering_angle(double vel, double omega, double wheelbase);
 
 
--- gz_io_ros/src/gz_io_ros_node.cpp
+++ gz_io_ros/src/gz_io_ros_node.cpp
@@ -19,26 +19,25 @@ double gir::GzIoRos::convert_trans_rot_vel_to_steering_angle(double vel, double 
 
 gir::GzIoRos::GzIoRos(rclcpp::NodeOptions options)
         : Node("gz_io_ros", options) {
-
+    rclcpp::QoS qos(50);
     _max_throttle_speed = this->declare_parameter("max_throttle_speed", 10.0);
     _max_braking_speed = this->declare_parameter("max_brake_speed", 10.0);
     _wheelbase = this->declare_parameter("wheelbase", 1.0);
 
     _odom_acks_pub = this->create_publisher<ackermann_msgs::msg::AckermannDrive>("/odom_ack", 10);
 
-    Subscriber<nav_msgs::msg::Odometry> odom_sub;
-    odom_sub.subscribe(this, "/odom");
+    _twist_sub = this->create_subscription<geometry_msgs::msg::Twist>("/robot/cmd_vel", rclcpp::QoS(10).reliable(),
+                                                                      std::bind(&GzIoRos::twist_cb, this,
+                                                                                std::placeholders::_1));
 
-    Subscriber<geometry_msgs::msg::TwistStamped> cmd_vel_sub;
-    cmd_vel_sub.subscribe(this, "/robot/cmd_vel");
-
-    Synchronizer<sync_policy> sync{sync_policy(10), odom_sub, cmd_vel_sub};
-    sync.registerCallback(std::bind(&gir::GzIoRos::convert_data, this, std::placeholders::_1, std::placeholders::_2));
+    _odom_sub = this->create_subscription<nav_msgs::msg::Odometry>("/odom", rclcpp::QoS(10).reliable(),
+                                                                   std::bind(&GzIoRos::odom_cb, this,
+                                                                             std::placeholders::_1));
 }
 
 void gir::GzIoRos::convert_data(nav_msgs::msg::Odometry::ConstSharedPtr odom,
-                                geometry_msgs::msg::TwistStamped::ConstSharedPtr cmd_vel) {
-    //Convert odom msg to ackermann drive msg
+                                geometry_msgs::msg::Twist::ConstSharedPtr twist) {
+    //Convert odom + twist msg to ackermann drive msg
     ackermann_msgs::msg::AckermannDrive msg;
     msg.steering_angle = this->convert_trans_rot_vel_to_steering_angle(odom->twist.twist.linear.x,
                                                                        odom->twist.twist.angular.z, this->_wheelbase);
@@ -48,8 +47,42 @@ void gir::GzIoRos::convert_data(nav_msgs::msg::Odometry::ConstSharedPtr odom,
     } else {
         msg.acceleration = odom->twist.twist.linear.x / _max_throttle_speed;
     }
-    msg.speed = cmd_vel->twist.linear.x;
+    msg.speed = twist->linear.x;
 
     this->_odom_acks_pub->get()->publish(msg);
-
 }
+
+void gir::GzIoRos::odom_cb(nav_msgs::msg::Odometry::SharedPtr odom) {
+    //Since twist messages aren't constant there will be situations where we have odom and no twist,
+    // in that case send zero twist to make sure those values are zeroed out
+    if (odom_queue.size() > 15) {
+        odom_queue.clear();
+    }
+    this->odom_queue.push_back(odom);
+
+    if (!this->odom_queue.empty()) {
+        if (this->twist_queue.empty()) {
+            convert_data(odom_queue.front(), std::make_shared<geometry_msgs::msg::Twist>(zero_twist));
+        } else {
+            convert_data(odom_queue.front(), twist_queue.front());
+            twist_queue.pop_front();
+        }
+        odom_queue.pop_front();
+    }
+}
+
+void gir::GzIoRos::twist_cb(geometry_msgs::msg::Twist::SharedPtr twist) {
+    //Since odom messages are constant as long as sim is running its fine to ensure both queues have data
+    if (twist_queue.size() > 15) {
+        twist_queue.clear();
+    }
+    this->twist_queue.push_back(twist);
+
+    if (!this->twist_queue.empty() && !this->odom_queue.empty()) {
+        convert_data(odom_queue.front(), twist_queue.front());
+        odom_queue.pop_front();
+        twist_queue.pop_front();
+    }
+}
+
+
