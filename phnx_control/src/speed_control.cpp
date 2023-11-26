597ff56cc314821f6d01137a105d8a0996f05b7f
Andrew Ealovega <Andrew@Ealovega.dev>
2023-11-26 01:28:16 -0500

    Fix a few things

--- phnx_control/src/speed_control.cpp
+++ phnx_control/src/speed_control.cpp
@@ -4,8 +4,8 @@ namespace phnx_control {
 
 SpeedController::SpeedController() {
     // TODO use real values
-    this->throttle_pid.initPid(0.2, 0.1, 0, 1, -1);
-    this->brake_pid.initPid(0.2, 0.1, 0, 1, -1);
+    this->throttle_pid.initPid(0.7, 0.5, 0, 1, 0);
+    this->brake_pid.initPid(0.7, 0.1, 0, 1, 0);
 }
 
 std::tuple<double, SpeedController::Actuator> SpeedController::update(double speed, const rclcpp::Time& stamp) {
@@ -20,14 +20,15 @@ std::tuple<double, SpeedController::Actuator> SpeedController::update(double spe
     double error = this->set_speed - speed;
 
     // If speed must be lowered by a large amount, use brakes
-    if (error <= -2) {
+    if (error <= -0.5) {
         if (!this->brake_active) {
             this->disable_throttle();
         }
 
         // TODO make command in 0-1 (I assume this is implicit in gains?)
+        // TODO currently command is always negative
         double command = this->brake_pid.computeCommand(error, dt);
-        return std::make_tuple(command, Actuator::Brake);
+        return std::make_tuple(-command, Actuator::Brake);
     }
     // Else, use throttle some amount
     else {
@@ -39,7 +40,7 @@ std::tuple<double, SpeedController::Actuator> SpeedController::update(double spe
 
         // Negative throttle values mean we should slow, but because we are on this path we have not used the brakes.
         // This means we should coast.
-        if (command < 0) {
+        if (command < 0.1) {
             command = 0;
         }
 
@@ -50,12 +51,12 @@ std::tuple<double, SpeedController::Actuator> SpeedController::update(double spe
 void SpeedController::update_set_speed(double speed) { this->set_speed = speed; }
 
 void SpeedController::disable_breaks() {
-    this->brake_pid.setCurrentCmd(0);  //TODO does this make sense to do? Or do we need a fake measurement
+    this->brake_pid.reset();
     this->brake_active = false;
 }
 
 void SpeedController::disable_throttle() {
-    this->throttle_pid.setCurrentCmd(0);
+    this->throttle_pid.reset();
     this->brake_active = true;
 }
 }  // namespace phnx_control
